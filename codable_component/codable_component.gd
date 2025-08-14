a553d66d275d2ccbb12ab3b5f6f1a632a0056986
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-08-14 16:26:11 -0700

    Removed print statement from CodableComponent

--- codable_component/codable_component.gd
+++ codable_component/codable_component.gd
@@ -5,6 +5,5 @@ class_name CodableComponent extends Area2D
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("edit"):
-		print(edited_script)
 		EditorManager.edit_script(edited_script)
 
