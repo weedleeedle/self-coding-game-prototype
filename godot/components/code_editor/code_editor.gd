9cfb88b52cb0fa72d56a85e13b494b4a849fc32e
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-08-15 19:26:36 -0700

    Added godot/ subfolder to organize things a lil better
    Also added a tileset ig, figuring out how that all works

---
+++ godot/components/code_editor/code_editor.gd
@@ -0,0 +1,26 @@
+class_name CodeEditor extends PanelContainer
+
+@export var edited_script: Script:
+	get:
+		return edited_script
+	set(value):
+		set_script_in_editor(value)
+		edited_script = value
+
+signal saved
+signal quit
+
+@onready var code_edit: CodeEdit = %CodeEdit
+
+func _ready() -> void:
+	pass
+
+func set_script_in_editor(script: Script) -> void:
+	code_edit.text = script.source_code
+
+func _on_quit_button_pressed() -> void:
+	quit.emit()
+
+func _on_save_button_pressed() -> void:
+	edited_script.source_code = code_edit.text
+	saved.emit()
