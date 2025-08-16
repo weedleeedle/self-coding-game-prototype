9616557fe79e0044c2bb64a92bc0455d17936add
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-08-15 19:45:09 -0700

    Got character movement working again hehe

--- godot/components/test_character/test_character.gd
+++ godot/components/test_character/test_character.gd
@@ -4,22 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _unhandled_input(event: InputEvent):
-	if event.is_action_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	if event.is_action_pressed("move_left") or event.is_action_pressed("move_right"):
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction = Input.get_axis("move_left", "move_right")
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+    if event.is_action_pressed("jump") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+    # Add the gravity.
+    if not is_on_floor():
+        velocity += get_gravity() * delta
 
-	move_and_slide()
+    velocity.x = Input.get_axis("move_left", "move_right") * SPEED
+
+    move_and_slide()
