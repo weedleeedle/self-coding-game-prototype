fa8cfffd67b4f4c5603140b08edf58ec61ddfa2d
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-08-18 21:01:39 -0700

    Code editor now saves and restores the state of objects at run time.
    This is such an incredibly cursed idea, this can't possibly be a good
    idea but at the same time it's incredibly cool that godot lets me do
    this lmao

--- godot/components/editor_manager/editor_manager.gd
+++ godot/components/editor_manager/editor_manager.gd
@@ -17,14 +17,31 @@ func _on_code_editor_saved():
     code_editor_layer.hide()
 
     var new_script = code_editor.edited_script
-    var script_owners: Array[Node] = []
+    # This will contain all of the nodes with the script as the key
+    # And all of the properties/state as the value. Wtf am I doing lol.
+    var script_owners: Dictionary = {}
     for child in get_tree().root.find_children("*", "", true, false):
         if new_script.instance_has(child):
-            print(child)
+            # Deep copy properties. :)
+            var properties = _get_object_properties(child)
+            script_owners.set(child, properties)
+            # Remove the script from the object.
             child.set_script(null)
-            script_owners.push_back(child)
-
 
     new_script.reload()
-    for script_owner in script_owners:
-        script_owner.set_script(new_script)
+    # Restore original state
+    for restore_object in script_owners:
+        restore_object.set_script(new_script)
+        var properties = script_owners[restore_object]
+        for property in properties:
+            restore_object.set(property, properties[property])
+
+## Returns a dictionary of properties.
+## Key is the property name, value is the current value.
+func _get_object_properties(object: Object) -> Dictionary:
+    var results := {}
+    for property in object.get_property_list():
+        var prop_value = object.get(property.name)
+        results.set(property.name, prop_value)
+
+    return results
