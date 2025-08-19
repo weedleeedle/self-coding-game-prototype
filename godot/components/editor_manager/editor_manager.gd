60c561df39e9cf510f4b7858137abd8b8715b8e0
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-08-18 19:44:25 -0700

    Added pausing to editor

--- godot/components/editor_manager/editor_manager.gd
+++ godot/components/editor_manager/editor_manager.gd
@@ -4,24 +4,27 @@ extends Node
 @onready var code_editor: CodeEditor = %CodeEditor
 
 func edit_script(script: Script) -> void:
-	code_editor.edited_script = script
-	code_editor_layer.show()
+    code_editor.edited_script = script
+    code_editor_layer.show()
+    get_tree().paused = true
 
 func _on_code_editor_quit():
-	code_editor_layer.hide()
+    get_tree().paused = false
+    code_editor_layer.hide()
 
 func _on_code_editor_saved():
-	code_editor_layer.hide()
+    get_tree().paused = false
+    code_editor_layer.hide()
 
-	var new_script = code_editor.edited_script
-	var script_owners: Array[Node] = []
-	for child in get_tree().root.find_children("*", "", true, false):
-		if new_script.instance_has(child):
-			print(child)
-			child.set_script(null)
-			script_owners.push_back(child)
+    var new_script = code_editor.edited_script
+    var script_owners: Array[Node] = []
+    for child in get_tree().root.find_children("*", "", true, false):
+        if new_script.instance_has(child):
+            print(child)
+            child.set_script(null)
+            script_owners.push_back(child)
 
 
-	new_script.reload()
-	for script_owner in script_owners:
-		script_owner.set_script(new_script)
+    new_script.reload()
+    for script_owner in script_owners:
+        script_owner.set_script(new_script)
