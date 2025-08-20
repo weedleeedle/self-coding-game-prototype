da3b7c0e872329c5410f1cb6272db2fdb912a8ec
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-08-20 15:44:19 -0700

    Added base code for handling invalid errors
    There's an inherent limitation where reloading a script with errors
    *will* halt execution and run the debugger. Ideally we would like to
    sandbox scripts and basically run a mini-debugger inside the game to
    evaluate the code and make sure it works. This may be significantly more
    challenging than I thought. There's ongoing work to stop reload() from
    halting the debugger, but nothing set in stone yet...

--- godot/components/code_editor/code_editor.gd
+++ godot/components/code_editor/code_editor.gd
@@ -1,26 +1,44 @@
 class_name CodeEditor extends PanelContainer
 
 @export var edited_script: Script:
-	get:
-		return edited_script
-	set(value):
-		set_script_in_editor(value)
-		edited_script = value
+    get:
+        return edited_script
+    set(value):
+        set_script_in_editor(value)
+        edited_script = value
 
 signal saved
 signal quit
 
 @onready var code_edit: CodeEdit = %CodeEdit
+@onready var error_descriptor: Label = %ErrorDescriptor
 
 func _ready() -> void:
-	pass
+    pass
 
 func set_script_in_editor(script: Script) -> void:
-	code_edit.text = script.source_code
+    code_edit.text = script.source_code
+
+## Tests a script with a new body of code.
+## Returns the Error enum, which is 0 if it's okay,
+func is_valid_script(script: Script, code: String) -> Error:
+    var new_script: GDScript = GDScript.new()
+    new_script.source_code = code
+    var error: Error = new_script.reload()
+    return error
 
 func _on_quit_button_pressed() -> void:
-	quit.emit()
+    quit.emit()
 
 func _on_save_button_pressed() -> void:
-	edited_script.source_code = code_edit.text
-	saved.emit()
+    var error = is_valid_script(edited_script, code_edit.text)
+    if error:
+        error_descriptor.show()
+        error_descriptor.text = "AAAAAAAAAA"
+        return
+
+    # If no error...
+    error_descriptor.hide()
+    edited_script.source_code = code_edit.text
+    saved.emit()
+
